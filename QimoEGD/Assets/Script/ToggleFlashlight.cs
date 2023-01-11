a09045f61403438cc63e9453354963bdd11d1b21
Tristanshalim <113578138+Tristanshalim@users.noreply.github.com>
2023-01-11 18:33:44 +0800

    UpdateGame

---
+++ QimoEGD/Assets/Script/ToggleFlashlight.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleFlashlight : MonoBehaviour
+{
+
+    public GameObject light;
+    public bool toggle;
+    public AudioSource toggleSound;
+
+    void Start()
+    {
+        if (toggle == false)
+        {
+            light.SetActive(false);
+        }
+        if (toggle == true)
+        {
+            light.SetActive(true);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            toggle = !toggle;
+            toggleSound.Play();
+            if (toggle == false)
+            {
+                light.SetActive(false);
+            }
+            if (toggle == true)
+            {
+                light.SetActive(true);
+            }
+        }
+    }
+}
